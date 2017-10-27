6e6a6a2ae7db131163cc396900e486700240c199
Hariz Yusoff <hrzyusoff@gmail.com>
2017-10-27 09:11:24 +0800

    Without urllib

--- Sites/carousell.py
+++ Sites/carousell.py
@@ -1,42 +1,30 @@
-from urllib.request import urlopen as uReq
+#from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+import requests
 
-userInput = 'noExit'
-while userInput != 'exit':
-	#userCategory = input('Enter category first : ')
-	#userKeyword = input('Enter keyword to search : ')
-	#frontMudahURL = 'https://shopee.com.my/search/?keyword=iphone'
-	#concatURL = frontMudahURL+userKeyword
-	my_url = 'https://carousell.com/search/products/?query=iphone'
+my_url = 'https://carousell.com/search/products/?query=iphone'
 
-	#opening up connection, grabbing the page
-	uClient = uReq(my_url)
-	page_html = uClient.read()
-	uClient.close()
+#headers used to treat like human request 
+#not using urllib because  it is like treat of bot
+headers = {'User-Agent':'Mozilla/5.0'}
+page = requests.get(my_url)
 
-	#html parsing
-	page_soup = soup(page_html, "html.parser")
+#html parsing
+page_soup = soup(page.text, "html.parser")
 
-	#filename = "mudah.csv"
-	#f = open(filename,"w")
-	#headers = "Brand, Price\n"
-	#f.write(headers)
+containers = page_soup.findAll("div",{"class":"row card-row"})
+gadgetname = page_soup.findAll("h4",{"class":"ProductCard__cardTitleContent___LESPy"})
+for container in gadgetname:
+	namelist = container.text
+	print(namelist)
 
-	containers = page_soup.findAll("div",{"class":"row card-row"})
+gadgetprice = page_soup.findAll("span",{"class":"ProductCard__cardPrice___1b7Lt"})
+for container in gadgetprice:
+	pricelist = container["title"]
+	print(pricelist)
 
-	for container in containers:
-		brandname = container.div.findAll("div",{"class":"ProductCard__cardTitleContent___LESPy"})
-
-		brandprice = container.div.findAll("div",{"class":"ProductCard__cardTitleContent___LESPy"})
-		brandpricelist = brandprice[0].text.strip()
-
-		print("Brand : "+brandname)
-		print("Price : "+brandpricelist)
-
-		#f.write(brandname + "," +brandpricelist+","+"\n")
-
-	#f.close()
-
-#userInput('Continue to search : ')
-
-#find('h1',attrs={'itemprop':'name'})
+#for container in containers:
+	#gadgetname = page_soup.findAll("h4",{"class":"ProductCard__cardTitleContent___LESPy"})
+	#gadgetprice = container.findAll("span",{"class":"ProductCard__cardPrice___1b7Lt"})
+	#print(gadgetname)
+	#print(gadgetprice)
--- Sites/lazada.py
+++ Sites/lazada.py
@@ -2,12 +2,12 @@
 from bs4 import BeautifulSoup as soup
 import requests
 
-my_url ='https://guardian.com.my/index.php/catalogsearch/result/?q=Tissue'
+my_url ='http://www.lazada.com.my/shop-mobiles/apple/?searchredirect=iphone'
 headers = {'User-Agent':'Mozilla/5.0'}
 page = requests.get(my_url)
 
 #html parsing
 page_soup = soup(page.text, "html.parser")
-phone = page_soup.findAll("h2",{"class":"product-name"})
+phone = page_soup.findAll("a",{"class":"c-product-card__name"})
 
 print(phone)
